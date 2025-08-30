94e90705d6f77d69f8a17e915a9226394fd95ef3
Jordan <jordanpraissman@gmail.com>
2025-08-30 19:04:06 -0400

    Init commit

---
+++ src/main/java/com/thelocalmusicfinder/thelocalmusicfinderbackend/TheLocalMusicFinderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thelocalmusicfinder.thelocalmusicfinderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLocalMusicFinderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLocalMusicFinderBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/thelocalmusicfinder/thelocalmusicfinderbackend/controller/EventController.java
@@ -0,0 +1,19 @@
+package com.thelocalmusicfinder.thelocalmusicfinderbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+
+  public EventController() {
+
+  }
+
+  @GetMapping
+  public String getEvents() {
+    return "These are all the events";
+  }
+}
