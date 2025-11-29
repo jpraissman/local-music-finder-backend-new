df86d65b7976c629af186bc9b71244c8c1e7b146
Jordan <jordanpraissman@gmail.com>
2025-11-29 13:59:37 -0500

    Add all genres

--- src/main/java/com/thelocalmusicfinder/thelocalmusicfinderbackend/domain/band/Genre.java
+++ src/main/java/com/thelocalmusicfinder/thelocalmusicfinderbackend/domain/band/Genre.java
@@ -1,6 +1,23 @@
 package com.thelocalmusicfinder.thelocalmusicfinderbackend.domain.band;
 
 public enum Genre {
+  ALTERNATIVE,
+  BLUES,
   CLASSIC_ROCK,
-  ROCK
+  COUNTRY,
+  DANCE,
+  DJ,
+  EDM,
+  FOLK,
+  HIP_HOP,
+  INDIE,
+  JAZZ,
+  KARAOKE,
+  LATIN,
+  METAL,
+  POP,
+  RNB_SOUL,
+  REGGAE,
+  ROCK,
+  WORLD_MUSIC
 }
