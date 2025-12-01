12b9a9ad68a70e08bb601dea03b8cc218f5472d8
Jordan <jordanpraissman@gmail.com>
2025-11-30 23:16:40 -0500

    Add base endpoints

--- src/main/java/com/thelocalmusicfinder/thelocalmusicfinderbackend/config/CorsConfig.java
+++ src/main/java/com/thelocalmusicfinder/thelocalmusicfinderbackend/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins(allowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
---
+++ src/main/java/com/thelocalmusicfinder/thelocalmusicfinderbackend/controllers/BaseController.java
@@ -0,0 +1,15 @@
+package com.thelocalmusicfinder.thelocalmusicfinderbackend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BaseController {
+
+  @GetMapping("/favicon.ico")
+  public ResponseEntity<Void> favicon() {
+    return ResponseEntity.noContent().build();
+  }
+
+}
